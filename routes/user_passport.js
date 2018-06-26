f753e47524fbf58f863ddbc005d1ffe0cd803c9d
James <ranmaru520000@gmail.com>
2018-06-26 11:36:25 +0900

    [Dapp-mapservice] Implement structure 1. Handle the flow of saving asset information

--- routes/user_passport.js
+++ routes/user_passport.js
@@ -12,7 +12,7 @@ module.exports = function (router, passport) {
     router.route('/login').get(function (req, res) {
         console.log('/login get routing called : ' + req.user);
         res.redirect('/login.html');
-     //   res.render('login.ejs', { message: req.flash('loginMessage') });
+        //   res.render('login.ejs', { message: req.flash('loginMessage') });
     });
 
     router.route('/login').post(passport.authenticate('local-login', {
@@ -21,12 +21,12 @@ module.exports = function (router, passport) {
         failureFlash: true
     }));
 
-    router.route('/loginsuccess').get(function(req, res) {
+    router.route('/loginsuccess').get(function (req, res) {
         console.log('/loginsuccess get routing called');
         res.send('<p>login success<br><a href="/profile">my page 로 이동</a></p>');
     });
 
-    router.route('/loginfailed').get(function(req, res) {
+    router.route('/loginfailed').get(function (req, res) {
         console.log('loginfailed get routing called');
         res.send('<p>login failed</p>');
     });
@@ -52,6 +52,17 @@ module.exports = function (router, passport) {
         res.render('map.ejs', { name: paramName, address: paramAddress, lat: paramLat, lng: paramLng });
     });
 
+    router.route('/saveassets1').post(function (req, res) {
+        var paramName = req.body.name || req.query.name;
+        var paramToken = req.body.token || req.query.token;
+        var paramLat = req.body.lat || req.query.lat;
+        var paramLng = req.body.lng || req.query.lng;
+
+        console.log('name : ' + paramName + ', ' + paramToken + ', ' + paramLat + ', ' + paramLng);
+        console.dir(req.body);
+        res.send({ result: false });
+    });
+
     router.route('/saveassets').post(function (req, res) {
         console.log('/saveassets post routing called');
 
@@ -69,31 +80,32 @@ module.exports = function (router, passport) {
 
             var pool = req.app.get('pool');
 
+            if (!paramName || !paramToken || !paramLat || !paramLng) {
+                res.send({ result: false, msg: '모든 항목을 입력하세요.' });
+                return;
+            }
+
             pool.getConnection(function (err, conn) {
                 if (err) {
                     if (conn) {
                         conn.release();
-
                     }
-                    res.writeHead(200, { "Content-Type": "text/html;charset=utf8" });
-                    res.write('<h1>save database connection error</h1>');
-                    res.end();
+                    res.send({ result: false, msg: '데이터베이스 연결실패.' });
                     return;
                 }
 
-                var data = {name:paramName, token:paramToken, latitude:paramLat, longitude:paramLng};
-                var exec = conn.query('insert into assets set ?', data, function(err, result) {
+                var data = { name: paramName, token: paramToken, latitude: paramLat, longitude: paramLng };
+                var exec = conn.query('insert into assets set ?', data, function (err, result) {
                     conn.release();
 
-                    if(err) {
+                    if (err) {
                         console.log('asset insert error');
+                        res.send({ result: false, msg: '저장 실패.' });
                         return;
                     }
 
                     console.log('실행된 SQL -> ' + exec.sql);
-                    res.writeHead(200, { "Content-Type": "text/html;charset=utf8"});
-                    res.write('<h1>' + exec.sql + ' success</h1>');
-                    res.end();
+                    res.send({ result: true, msg: '저장 성공.' });
                 });
             });
         }
@@ -117,45 +129,45 @@ module.exports = function (router, passport) {
 
             var pool = req.app.get('pool');
 
-            pool.getConnection(function(err, conn) {
-                if(err) {
+            pool.getConnection(function (err, conn) {
+                if (err) {
                     conn.release();
                     console.log('get profile connection error');
                     return;
                 }
 
-                var exec = conn.query("select * from assets", [], 
-                function(err, rows) {
-                    conn.release();
-                    
-    
-                    if(err) {
-                        console.log('select assets error');
-                        return;
-                    }
-    
-                    console.log('실행된 SQL -> ' + exec.sql);
-                    var assetList = rows;
-    
-                    for (var i = 0; i < assetList.length; i++) {
-               //         console.log('#' + i + ' -> ' + assetList[i].name + ', ' + assetList[i].address);
-                        console.dir(assetList[i]);
-                    }
-        
-                    if (Array.isArray(req.user)) {
-                        console.log('user is array');
-                        console.dir(req.user[0]);
-                        userInfo = req.user[0];
-                        //res.render('profile.ejs', { assetList: assetList });
-                        res.render('admin.ejs', { assetList: assetList });
-                    } else {
-                        console.log('user is not array');
-                        console.dir(req.user);
-                        userInfo = req.user;
-                        //res.render('profile.ejs', { user: req.user });
-                    }
-                });
-    
+                var exec = conn.query("select * from assets", [],
+                    function (err, rows) {
+                        conn.release();
+
+
+                        if (err) {
+                            console.log('select assets error');
+                            return;
+                        }
+
+                        console.log('실행된 SQL -> ' + exec.sql);
+                        var assetList = rows;
+
+                        for (var i = 0; i < assetList.length; i++) {
+                            //         console.log('#' + i + ' -> ' + assetList[i].name + ', ' + assetList[i].address);
+                            console.dir(assetList[i]);
+                        }
+
+                        if (Array.isArray(req.user)) {
+                            console.log('user is array');
+                            console.dir(req.user[0]);
+                            userInfo = req.user[0];
+                            //res.render('profile.ejs', { assetList: assetList });
+                            res.render('admin.ejs', { assetList: assetList });
+                        } else {
+                            console.log('user is not array');
+                            console.dir(req.user);
+                            userInfo = req.user;
+                            //res.render('profile.ejs', { user: req.user });
+                        }
+                    });
+
             });
 
             // console.log('user grade : ' + userInfo.grade);
